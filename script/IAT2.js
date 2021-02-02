50b1ff54197944c19955ab371841710ef4838b39
teri-trevains <trevainst@cardiff.ac.uk>
2021-02-02 11:22:41 +0000

    fix mis-paste

--- script/IAT2.js
+++ script/IAT2.js
@@ -2,14 +2,15 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
     var API = new APIConstructor();
 
 	return iatExtension({
-		category1 : {
-            remindErrorText : '<p align="center" style="font-size:"1.0em"; font-family:arial">' +
-                'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
-                'Press the <b>other key</b> to continue.<p/>',
+        remindErrorText : '<p align="center" style="font-size:"1.0em"; font-family:arial">' +
+            'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
+            'Press the <b>other key</b> to continue.<p/>',
 
-            remindErrorTextTouch : '<p align="center" style="font-size:"1.4em"; font-family:arial">' +
-                'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
-                'Touch the <b>other side</b> to continue.<p/>',
+        remindErrorTextTouch : '<p align="center" style="font-size:"1.4em"; font-family:arial">' +
+            'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
+            'Touch the <b>other side</b> to continue.<p/>',
+		category1 : {
+
 			name : 'Civilian', //Will appear in the data.
 			title : {
 				media : {word : 'Civilian'}, //Name of the category presented in the task.
