e7674497f0753179f4d50d3bc5693f59b7b28c68
teri-trevains <trevainst@cardiff.ac.uk>
2021-02-02 11:34:19 +0000

    change to base IAT

--- script/IAT3.js
+++ script/IAT3.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://teri-trevains.github.io/IAT1/script/base.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
